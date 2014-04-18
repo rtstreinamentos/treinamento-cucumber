d586915313e14b4174fc4cc467f944329196fe20
tghcastro <tghcastro@gmail.com>
2014-04-18 10:43:18 -0300

    - Criação dos testes para a Newsletter - Definição dos Steps para a Newsletter

---
+++ src/test/features/rts/treinamento/pesquisa/CadastroDeNewsletter.feature
@@ -0,0 +1,22 @@
+# language: pt
+Funcionalidade: Cadastrar um e-mail para receber Newsletter
+  Como cliente
+  Para receber promoções 
+  Quero cadastra meu e-mail para receber as Newsletter
+
+  Contexto: 
+    Dado que a Newsletter esteja ativada
+
+  Cenário: Cadastrar um e-mail válido não cadastrado
+    Dado que meu e-mail ainda não foi cadastrado na newsletter
+    Quando cadastro meu e-mail
+    Então visualizo a mensagem de sucesso do cadastro da newsletter
+
+  Cenário: Tentar cadastrar e-mail já cadastrado
+    Dado que meu e-mail ainda já foi cadastrado na newsletter
+    Quando cadastro meu e-mail
+    Então visualizo a mensagem informando que o e-mail já havia sido cadastrado
+
+  Cenário: Tentar cadastrar e-mail inválido
+    Quando tento cadastrar um e-mail inválido
+    Então não consigo cadastrar o email na newsletter
---
+++ src/test/java/rts/treinamento/CadastroDeNewsLetterSteps.java
@@ -0,0 +1,59 @@
+package rts.treinamento;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class CadastroDeNewsLetterSteps {
+	@Dado("^que a Newsletter esteja ativada$")
+	public void que_a_Newsletter_esteja_ativada() throws Throwable {
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+
+	@Dado("^que meu e-mail ainda não foi cadastrado na newsletter$")
+	public void que_meu_e_mail_ainda_não_foi_cadastrado_na_newsletter()
+			throws Throwable {
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+
+	@Quando("^cadastro meu e-mail$")
+	public void cadastro_meu_e_mail() throws Throwable {
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+
+	@Então("^visualizo a mensagem de sucesso do cadastro da newsletter$")
+	public void visualizo_a_mensagem_de_sucesso_do_cadastro_da_newsletter()
+			throws Throwable {
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+	
+	@Dado("^que meu e-mail ainda já foi cadastrado na newsletter$")
+	public void que_meu_e_mail_ainda_já_foi_cadastrado_na_newsletter() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Então("^visualizo a mensagem informando que o e-mail já havia sido cadastrado$")
+	public void visualizo_a_mensagem_informando_que_o_e_mail_já_havia_sido_cadastrado() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+	@Quando("^tento cadastrar um e-mail inválido$")
+	public void tento_cadastrar_um_e_mail_inválido() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Então("^não consigo cadastrar o email na newsletter$")
+	public void não_consigo_cadastrar_o_email_na_newsletter() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+}
